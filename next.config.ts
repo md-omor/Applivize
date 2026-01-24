0b4dab161d95c8d6a582513cb045424dc244d425
Your Name <developeromor@gmail.com>
2026-01-25 02:45:49 +0600

    edit next confid

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pdfjs-dist", "pdf-parse"],
+  outputFileTracingIncludes: {
+    "/api/upload": ["./node_modules/pdf-parse/dist/pdf-parse/cjs/pdf.worker.mjs"],
+    "/api/analyze-files": ["./node_modules/pdf-parse/dist/pdf-parse/cjs/pdf.worker.mjs"],
+  },
 };
 
 export default nextConfig;
